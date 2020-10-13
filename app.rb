dff55351f42796fe5a209dab9bcae59c4032fde5
doosemavis <moosedavis2011@gmail.com>
2020-10-13 15:29:45 -0500

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,22 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+    #sets up homepage
+    get '/' do
+        "Hello, World!"
+    end 
+
+    get '/name' do
+        "My name is Moose Davis"
+    end 
+
+    get '/hometown' do
+        "My hometown is Jonesboro, Arkansas"
+    end 
+
+    get '/favorite-song' do
+        "My favorite song is How You Like That, by BLACKPINK"
+    end 
+
 end
